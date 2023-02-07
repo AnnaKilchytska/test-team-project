84a9960f1d5c5c352ee7f4769dde184cb52c9e5c
Anna <80330820+AnnaKilchytska@users.noreply.github.com>
2023-02-07 16:39:46 +0200

    make styles and functionality update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ const API_KEY = '5e62b2453973c629f1496ace3a4b7719';
 // const API_KEY = '60bdd84997c9f2a4c6cd2341c547ed98';
 const BASE_URL = 'https://api.themoviedb.org/3/trending/movie/day';
 
-const cardList = document.querySelector('.cards-list');
+const cardList = document.querySelector('.trending-gallery');
 
 async function fetchPopularFilms() {
   try {
     const data = await fetch(`${BASE_URL}?api_key=${API_KEY}`);
     const parcedData = await data.json();
-    // console.log(films);
-    // console.log(films.results[0].genre_ids);
     const { results } = parcedData;
     return results;
   } catch (error) {
@@ -32,78 +30,87 @@ async function getImages() {
   return imageBaseURL;
 }
 
-async function getGenres(ids) {
+async function getGenres() {
   const data = await fetch(
     `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
   );
-
+  // отримуємо назви жанрів
   const genresData = await data.json();
   const { genres } = genresData;
   console.log(genres);
 
-  let selectedGenres = [];
+  // масив об'єктів з властивістю id i значенням name
 
-  genres.filter(item => {
-    if (ids.includes(item.id)) {
-      console.log(item.name);
-      selectedGenres.push(item.name);
-      return item.name;
-    }
+  const genreNames = {};
+
+  genres.map(item => {
+    genreNames[item.id] = item.name;
   });
-  console.log(selectedGenres);
-  return selectedGenres;
+
+  console.log('genreNames', genreNames);
+
+  return genreNames;
 }
 
 fetchPopularFilms();
 getImages();
 
-async function createMarkup(data, genres) {
+async function createMarkup(data) {
   console.log('data results', data);
   //   const imageBaseURL = await getImages();
 
-  // ПРОБА ПЕРА
+  const genreNames = await getGenres();
+  console.log('genreNames in markup', genreNames);
+
   const imageBaseURL = 'https://image.tmdb.org/t/p/original';
   console.log(imageBaseURL);
 
-  //   const genreIDs = getGenreIDs(data);
-  //   console.log('genre ids', genreIDs);
-
   const markup = data
     .map(item => {
+      let genres = '';
+      const genresNamesToRender = getGenreDeciphered(item, genreNames);
+      if (genresNamesToRender.includes('Science Fiction')) {
+        genres = `${genresNamesToRender[0]}, Other`;
+      } else if (genresNamesToRender.length > 2) {
+        genres = `${genresNamesToRender[0]}, ${genresNamesToRender[1]}, Other`;
+      } else {
+        genres = genresNamesToRender;
+      }
+
       console.log('item', item);
-
-      //   const genres = getGenres(genreIDs).then(res => {
-      //     return res;
-      //   });
-      //   console.log('genres', genres);
-
       return `<li>
-    <img src="${imageBaseURL}${item.poster_path}" />
-    <p>${genres}</p>
+    <img src="${imageBaseURL}${item.poster_path}" 
+          alt="The poster of ${item.title} film" 
+          class="trending-gallery__image" />
+    <div class="trending-gallery__wrapper">
+    <h3 class="trending-gallery__title">${item.title}</h3>
+    <p class="trending-gallery__info">${genres} | <span>${item.release_date.slice(
+        0,
+        4
+      )}</span></p>
+    </div>
     </li>`;
     })
     .join('');
   return markup;
 }
 
-function getGenreIDs(array) {
-  const repeatedGenres = array.flatMap(item => {
-    return item.genre_ids;
+function getGenreDeciphered(filmObject, genresList) {
+  const genreArray = filmObject.genre_ids;
+
+  const genreNamesToRender = genreArray.map(item => {
+    return genresList[item];
   });
-  const genres = new Set(repeatedGenres);
-  const genreArray = Array.from(genres);
-  return genreArray;
+
+  console.log('genresToRender', genreNamesToRender);
+  return genreNamesToRender;
 }
 
 async function renderPopularFilms() {
   try {
     const films = await fetchPopularFilms();
     console.log(films);
-    const genreIDs = getGenreIDs(films);
-    console.log('genre ids', genreIDs);
-    const genres = await getGenres(genreIDs);
-    console.log('genres', genres);
-    const markup = await createMarkup(films, genres);
+    const markup = await createMarkup(films);
     console.log(films);
     console.log(films.genre_ids);
     // console.log(markup);
